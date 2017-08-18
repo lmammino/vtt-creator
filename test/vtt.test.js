207ad8efe3b48a711b2b71a768f4157344da1e45
Cameron Fraser <camfraser@me.com>
2017-08-18 12:52:35 -0600

    Add an optional settings parameter to add() (#3)
    * Add an optional settings parameter to add()
    
    * Removing end-of-life node versions from travis https://github.com/nodejs/LTS

--- test/vtt.test.js
+++ test/vtt.test.js
@@ -20,3 +20,12 @@ test('It must create a vtt with content', function (t) {
   v.add(5, 9, ['It will perforate your stomach.', 'You could die.']);
   t.equals(v.toString(), expected);
 });
+
+test('It must create a vtt with settings', function (t) {
+  t.plan(1);
+  var expected = fs.readFileSync(path.join(__dirname, 'fixtures', 'sample-settings.vtt'), 'utf8');
+  var v = new Vtt();
+  v.add(1.05, 4, 'Never drink liquid nitrogen.', 'position:10%,line-left align:left size:35%');
+  v.add(5, 9, ['It will perforate your stomach.', 'You could die.'], 'position:90% align:right size:35%');
+  t.equals(v.toString(), expected);
+});
