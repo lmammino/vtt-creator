85e7c2145d9939e437c5b7a3894c8e215d3c7755
Luciano Mammino <lucianomammino@gmail.com>
2017-08-18 19:55:16 +0100

    Added package-lock, new node versions in CI and version bump

--- src/vtt.js
+++ src/vtt.js
@@ -6,15 +6,11 @@ module.exports = function () {
   var counter = 0;
   var content = 'WEBVTT\r\n';
 
-  this.add = function (from, to, lines, settings) {
+  this.add = function (from, to, lines) {
     ++counter;
     lines = lines.constructor === Array ? lines : [lines];
 
-    content += '\r\n' + counter + '\r\n' +
-      secondsToTime(from) + ' --> ' + secondsToTime(to) +
-      (settings ? ' ' + settings : '') +
-      '\r\n';
-
+    content += '\r\n' + counter + '\r\n' + secondsToTime(from) + ' --> ' + secondsToTime(to) + '\r\n';
     lines.forEach(function (line) {
       content += line + '\r\n';
     });
