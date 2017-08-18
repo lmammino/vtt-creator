d4c4b95efb9c506217ce56eb10b4f39d94d935e1
Cameron Fraser <camfraser@me.com>
2017-08-18 12:52:35 -0600

    Add an optional settings parameter to add() (#3)
    * Add an optional settings parameter to add()
    
    * Removing end-of-life node versions from travis https://github.com/nodejs/LTS

--- src/vtt.js
+++ src/vtt.js
@@ -6,11 +6,15 @@ module.exports = function () {
   var counter = 0;
   var content = 'WEBVTT\r\n';
 
-  this.add = function (from, to, lines) {
+  this.add = function (from, to, lines, settings) {
     ++counter;
     lines = lines.constructor === Array ? lines : [lines];
 
-    content += '\r\n' + counter + '\r\n' + secondsToTime(from) + ' --> ' + secondsToTime(to) + '\r\n';
+    content += '\r\n' + counter + '\r\n' +
+      secondsToTime(from) + ' --> ' + secondsToTime(to) +
+      (settings ? ' ' + settings : '') +
+      '\r\n';
+
     lines.forEach(function (line) {
       content += line + '\r\n';
     });
